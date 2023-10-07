d5a3502b037eef620eb9f89575c348ef3aef3635
thomaskitaba <thomaskitaba@gmail.com>
2023-10-07 22:08:04 +0300

    task 3 almost finished

--- web_dynamic/static/scripts/3-hbnb.js
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -8,7 +8,34 @@ $('document').ready(function () {
       $('DIV#api_status').removeClass('available');
     }
   });
+  $.ajax({
+    url: api + ':5001/api/v1/places_search/',
+    type: 'POST',
+    data: '{}',
+    contentType: 'application/json',
+    dataType: 'json',
+    success: function (data) {
+        $("section.places").append(data.map(place => {
+        return `<article>
+        <div class="title_box">
+          <h2>${place.name}</h2>
+          <div class="price_by_night">${place.price_by_night }</div>
+        </div>
+        <div class="information">
+          <div class="max_guest">${place.max_guest} Guests</div>
+              <div class="number_rooms">${place.number_rooms} Bedrooms </div>
+              <div class="number_bathrooms">${place.number_bathrooms} Bathrooms </div>
+        </div>
+            <div class="description">
+          ${place.description}
+            </div>
+      </article>`;
 
+
+
+    }));
+    }
+  });
     let amenities = {};
     $('INPUT[type="checkbox"]').change(function () {
       if ($(this).is(':checked')) {
