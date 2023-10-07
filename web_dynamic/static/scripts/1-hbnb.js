de3bc9e7bd8d677eefb25a6ccd6c56bc6c9b2aee
thomaskitaba <thomaskitaba@gmail.com>
2023-10-07 16:20:01 +0300

    added files

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,3 +1,13 @@
-$("document").ready( function () {
 
+$("document").ready( function () {
+    let amenity = {}
+    $('INPUT[type="checkbox"]').change(function (){
+        if ($(this).is(":checked")) {
+            amenity[$(this).attr('data-id')] = $(this).attr('data-name');
+        }
+        else {
+            delete amenity[$(this).attr('data-id')];
+        }
+    });
+    $(".amenities H4").text(Object.value(amenity), join(", "));
 });
